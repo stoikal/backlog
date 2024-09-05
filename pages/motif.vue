04cf81544f6418611ea33c4f5668d0240b3ecdb9
Laksito Pamilih <laksito@protonmail.com>
2024-09-05 23:31:04 +0700

    feat(motif): onnly show 3

--- pages/motif.vue
+++ pages/motif.vue
@@ -2,38 +2,65 @@
 const motifs = [
   {
     id: 1,
-    name: 'Thread 1',
+    name: 'Motif 1',
     nodes: [
-      { id: 3, date: '2024/08/30', next: null, done: ['c', 'd'], todo: ['e', 'f'] },
-      { id: 2, date: '2024/08/20', next: 3, done: ['a', 'b'], todo: ['c', 'd'] },
-      { id: 1, date: '2024/08/10', next: 2, todo: ['a', 'b'] },
+      { id: 3, date: '2024/08/30', next: null, done: ['iterate c', 'research d'], todo: ['do e', 'work on f'] },
+      { id: 2, date: '2024/08/20', next: 3, done: ['work on a', 'do b'], todo: ['iterate c', 'research d'] },
+      { id: 1, date: '2024/08/10', next: 2, todo: ['work on a', 'do b'] },
     ]
   },
   {
     id: 2,
-    name: 'Thread 2',
+    name: 'Motif 2',
     nodes: [
     { id: 2, date: '2024/08/31', next: 3, done: ['a', 'b'], todo: ['c', 'd'] },
     { id: 1, date: '2024/08/29', next: 2, todo: ['a', 'b'] },
     ]
   },
+  {
+    id: 3,
+    name: 'Motif 3',
+    nodes: [
+    { id: 3, date: '2024/08/30', next: null, done: ['c', 'd'], todo: ['e', 'f'] },
+      { id: 2, date: '2024/08/20', next: 3, done: ['a', 'b'], todo: ['c', 'd'] },
+      { id: 1, date: '2024/08/10', next: 2, todo: ['a', 'b'] },
+    ]
+  },
+  {
+    id: 4,
+    name: 'Motif 4',
+    nodes: [
+    { id: 3, date: '2024/08/30', next: null, done: ['c', 'd'], todo: ['e', 'f'] },
+      { id: 2, date: '2024/08/20', next: 3, done: ['a', 'b'], todo: ['c', 'd'] },
+      { id: 1, date: '2024/08/10', next: 2, todo: ['a', 'b'] },
+    ]
+  },
 ]
+
+const shownStartIndex = ref(0)
 </script>
 
 <template>
+  <button
+    :disabled="shownStartIndex === 0"
+    @click="shownStartIndex--"
+  >
+    <
+  </button>
   <div
     style="width: 1000px; margin: 0 auto;"
   >
     <a-row justify="center" :gutter="16">
-      <template v-for="motif in motifs">
+      <template v-for="motif in motifs.slice(shownStartIndex, shownStartIndex + 3)">
         <a-col :span="8">
           {{  motif.name }}
           <template v-for="node in motif.nodes" :key="node.id">
-            <a-card
-              :title="node.id"
-            >
+            <a-card>
+              <template #extra>
+                {{ node.date }}
+              </template>
               <template v-if="node.done?.length > 0">
-                DONE
+                PREV
                 <ul>
                   <template v-for="item in node.done">
                     <li>{{ item }}</li>
@@ -55,4 +82,10 @@ const motifs = [
       </template>
     </a-row>
   </div>
+  <button
+    :disabled="shownStartIndex >= motifs.length - 3"
+    @click="shownStartIndex++"
+  >
+    >
+  </button>
 </template>
