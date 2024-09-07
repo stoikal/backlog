fd7d6a081efb8dad224096d64a4b5115d7badfc1
Laksito Pamilih <laksito@protonmail.com>
2024-09-07 12:13:09 +0700

    revamp-ui: install shadcn-vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@ant-design-vue/nuxt', '@nuxtjs/supabase'],
+  modules: [
+    '@ant-design-vue/nuxt',
+    '@nuxtjs/supabase',
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt'
+  ],
   css: ['ant-design-vue/dist/reset.css']
 })
