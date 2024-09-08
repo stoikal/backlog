4fe03cae4c6b6844cf8f38558a6bb81b5e09c76d
Laksito Pamilih <laksito@protonmail.com>
2024-09-08 17:37:00 +0700

    feat(reading-backlog): add link to index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <NuxtLink to="/games">
-    games backlog
-  </NuxtLink>
+  <div class="max-w-[1200px] mx-auto p-6">
+    <ul class="space-y-2">
+      <li>
+        <NuxtLink to="/reading" class="hover:underline">
+          reading backlog
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/games" class="hover:underline">
+          gaming backlog
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
 </template>
